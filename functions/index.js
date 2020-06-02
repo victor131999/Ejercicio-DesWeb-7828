daa5318be77b37eb1d04fdfb008ef9c6a3c71fec
victor131999 <58785449+victor131999@users.noreply.github.com>
2020-06-01 21:17:40 -0500

    REST API Firebase Cloud functions JS Realtime Database

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,94 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin'); //Todo el SDK de Firebase => Acceso a Realtime Database
 const express = require('express');
 const app = express();
 
-//credenciales agregadas//
-var firebaseConfig = {
-  apiKey: "AIzaSyCpvO7wTr4VYm4U3U-cm3-bOxlTfIqVk3g",
-  authDomain: "proyecto-1-nrc-7828.firebaseapp.com",
-  databaseURL: "https://proyecto-1-nrc-7828.firebaseio.com",
-  projectId: "proyecto-1-nrc-7828",
-  storageBucket: "proyecto-1-nrc-7828.appspot.com",
-  messagingSenderId: "150082542761",
-  appId: "1:150082542761:web:5a26bc0e25ba54dc77ddb8"
-};
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+var serviceAccount = require("../serviceAccountKey.json");
 
-
-  //exploramos funciones
-exports.helloWorld = functions.https.onRequest((request, response) => {
-response.send("Proyexto 1 NRC: 7828");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://proyecto-1-nrc-7828.firebaseio.com"
 });
 
-app.get('/', function(req, res){
-    res.send('El primer proyecto de Desarrollo Web Avanzado')
+const database = admin.database();
+
+///========================= Variables globales ===================///
+const dbPeople = "persons"; //Referencia al nodo en donde se van a guardar las personas
+
+///========================= Métodos internos ===================///
+function createPerson(person){
+  database.ref(dbPeople).push(person);  
+}
+
+function retrievePerson(id){
+  return database.ref(dbPeople).child(id).once('value');
+}
+
+function updatePerson(id, person){
+  database.ref(dbPeople).child(id).set(person);
+}
+
+function deletePerson(id){
+  database.ref(dbPeople).child(id).remove();
+}
+
+function listPersons(){
+  return database.ref(dbPeople).once('value');
+}
+
+///========================= Funciones URLs ===================///
+app.post('/api/persons', function (req, res) {
+  let varName = req.body['name'];
+  let varAge = req.body['age'];
+  var person = {
+    name : varName,
+    age : varAge  };
+  createPerson(person);
+  return res.status(201).json({ message: "Success person was added." });
+});
+
+app.get('/api/persons/:id', function(req, res){
+  let varId = req.params.id;
+  retrievePerson(varId).then(result => {
+      return res.status(200).json(result); 
+    }
+  ).catch(err => console.log(err));
+});
+
+app.put('/api/persons/:id', function (req, res) {
+  let varId = req.params.id;
+  let varName = req.body['name'];
+  let varAge = req.body['age'];
+  var person = {
+    name : varName,
+    age : varAge  };
+  updatePerson(varId, person);
+  return res.status(200).json({ message: "Success person was updated." });
+});
+
+app.delete('/api/persons/:id',function(req, res){
+  let varId = req.params.id;
+  deletePerson(varId);
+  return res.status(200).json({ message: "Success person was deleted." });
+});
+
+app.get('/api/persons', function(req, res){
+  listPersons().then(result => {
+      return res.status(200).json(result); 
+    }
+  ).catch(err => console.log(err));
+});
+
+app.get('/api/person', function(req, res){
+  let varId = req.query.id;
+  retrievePerson(varId).then(result => {
+      return res.status(200).json(result); 
+   }
+  ).catch(err => console.log(err));
+});
+
+app.get('/api/', function (req, res) {
+  res.send('Bienvenid@s a Cloud Functions de Desarrollo Web Avanzado NRC 7828')
 })
 
 exports.app = functions.https.onRequest(app);
